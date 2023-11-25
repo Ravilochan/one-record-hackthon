351574b4f9c80ce7c5a139c1004f2b510929f413
Ravilochan <ravilochan2000@gmail.com>
2023-11-25 12:52:58 +0300

    fixed background image

--- frontend/src/common/footer/Footer.jsx
+++ frontend/src/common/footer/Footer.jsx
@@ -4,11 +4,11 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <React.Fragment>
-      <div className="Footer__wrapper">
-        <div className="mi_page_container">
-          <div className="Footer__space">
-            <p className="Footer__body_fs">Copyright © Airline Portal</p>
-            <p className="Footer__body_fs">@2023</p>
+      <div className='Footer__wrapper'>
+        <div className='mi_page_container'>
+          <div className='Footer__space'>
+            <p className='Footer__body_fs'>Copyright © Airline Portal</p>
+            <p className='Footer__body_fs'>@2023</p>
           </div>
         </div>
       </div>
