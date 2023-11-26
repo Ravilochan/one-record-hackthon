5ebbc68ca0b66fa1376cbb68b879bb76eacb773e
Ravilochan <ravilochan2000@gmail.com>
2023-11-26 08:55:38 +0300

    Price list update

--- frontend/src/components/PriceList/PriceList.jsx
+++ frontend/src/components/PriceList/PriceList.jsx
@@ -17,97 +17,124 @@ export default function PriceList(props) {
   const { onClose, open, handleOpenReviewCOR, ...other } = props;
 
   return (
-    <React.Fragment>
-      <Dialog
-        sx={{ "& .MuiDialog-paper": { width: "100%" } }}
-        maxWidth='sm'
-        open={open}
-        TransitionComponent={Transition}
-        {...other}
-      >
-        <DialogTitle>
-          <div className='ModalPage__header'>
-            <h1 className='ModalPage_main_title_fs'>
-              Pick the Flight and Price
-            </h1>
-            <div
-              className='ModalPage__close_icon'
-              onClick={() => {
-                onClose();
-              }}
-            >
-              <i className='bx bx-x'></i>
-            </div>
+    <Dialog
+      sx={{ "& .MuiDialog-paper": { width: "100%" } }}
+      maxWidth='sm'
+      open={open}
+      TransitionComponent={Transition}
+      {...other}
+    >
+      <DialogTitle>
+        <div className='ModalPage__header'>
+          <h1 className='ModalPage_main_title_fs'>Pick the Flight and Price</h1>
+          <div
+            className='ModalPage__close_icon'
+            onClick={() => {
+              onClose();
+            }}
+          >
+            <i className='bx bx-x'></i>
           </div>
-        </DialogTitle>
-        <DialogContent dividers>
-          <div className='SaveDraft__list'>
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-            <FlightPriceList />
-          </div>
-        </DialogContent>
-        <DialogActions>
-          <div className='ModalPage__actions'>
-            <button
-              className='mi_btn mi_btn_medium mi_btn_primary'
-              onClick={() => {
-                onClose();
-              }}
-            >
-              <span>Close</span>
-            </button>
-          </div>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+        </div>
+      </DialogTitle>
+      <DialogContent dividers>
+        <div className='SaveDraft__list'>
+          <FlightPriceList
+            flightDate='26/11/2023'
+            flightNo='QR38'
+            price='$11/kg'
+            onClose={onClose}
+          />
+          <FlightPriceList
+            flightDate='26/11/2023'
+            flightNo='QR40'
+            price='$7.8/kg'
+            onClose={onClose}
+          />
+          <FlightPriceList
+            flightDate='26/11/2023'
+            flightNo='QR42'
+            price='$7.2/kg'
+            onClose={onClose}
+          />
+          <FlightPriceList
+            flightDate='27/11/2023'
+            flightNo='QR38'
+            price='$6.0/kg'
+            onClose={onClose}
+          />
+          <FlightPriceList
+            flightDate='27/11/2023'
+            flightNo='QR42'
+            price='$6.0/kg'
+            onClose={onClose}
+          />
+          <FlightPriceList
+            flightDate='28/11/2023'
+            flightNo='QR40'
+            price='$6.4/kg'
+            onClose={onClose}
+          />
+          <FlightPriceList
+            flightDate='28/11/2023'
+            flightNo='QR38'
+            price='$4.5/kg'
+            onClose={onClose}
+          />
+          <FlightPriceList
+            flightDate='28/11/2023'
+            flightNo='QR42'
+            price='$4.0/kg'
+            onClose={onClose}
+          />
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <div className='ModalPage__actions'>
+          <button
+            className='mi_btn mi_btn_medium mi_btn_primary'
+            onClick={() => {
+              onClose();
+            }}
+          >
+            <span>Close</span>
+          </button>
+        </div>
+      </DialogActions>
+    </Dialog>
   );
 }
 
-function FlightPriceList() {
+function FlightPriceList(props) {
   return (
-    <React.Fragment>
-      <div className='SaveDraftItems__card'>
-        <div className='SaveDraftItems__carting_info'>
-          <div className='CartingTableData__wrap'>
-            <p className='mi_tiny TableData__heading'>Flight Number</p>
-            <h1 className='mi_small TableData__bodyInfo'>161223728</h1>
-          </div>
-          <div className='CartingTableData__wrap'>
-            <p className='mi_tiny TableData__heading'>Flight Date</p>
-            <h1 className='mi_small TableData__bodyInfo'>25/11/2023</h1>
-          </div>
-          <div className='CartingTableData__wrap'>
-            <p className='mi_tiny TableData__heading'>Destination</p>
-            <div className='DestinationTableData__location_wrap'>
-              <h1 className='mi_small TableData__bodyInfo'>HYD</h1>
-              <img src={Arrow} alt='Arrow' />
-              <h1 className='mi_small TableData__bodyInfo'>UAE</h1>
-            </div>
-          </div>
+    <div className='SaveDraftItems__card'>
+      <div className='SaveDraftItems__carting_info'>
+        <div className='CartingTableData__wrap'>
+          <p className='mi_tiny TableData__heading'>Flight Number</p>
+          <h1 className='mi_small TableData__bodyInfo'>{props.flightNo}</h1>
         </div>
-
-        <div className='SaveDraftItems__actions'>
-          <button className={`mi_btn mi_btn_medium mi_btn_accept`}>
-            <span>Rs 12,000</span>
-          </button>
+        <div className='CartingTableData__wrap'>
+          <p className='mi_tiny TableData__heading'>Flight Date</p>
+          <h1 className='mi_small TableData__bodyInfo'>{props.flightDate}</h1>
+        </div>
+        <div className='CartingTableData__wrap'>
+          <p className='mi_tiny TableData__heading'>Destination</p>
+          <div className='DestinationTableData__location_wrap'>
+            <h1 className='mi_small TableData__bodyInfo'>CDG</h1>
+            <img src={Arrow} alt='Arrow' />
+            <h1 className='mi_small TableData__bodyInfo'>DOH</h1>
+          </div>
         </div>
       </div>
-    </React.Fragment>
+
+      <div className='SaveDraftItems__actions'>
+        <button
+          className={`mi_btn mi_btn_medium mi_btn_accept`}
+          onClick={() => props.onClose()}
+        >
+          <span>{props.price}</span>
+        </button>
+      </div>
+    </div>
   );
 }
